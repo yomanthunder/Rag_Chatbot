3235b2db51ee987b618b53dba35b554df4313675
Shivakamalesh03 <shivakamalesh45@gmail.com>
2025-04-04 23:52:02 +0530

    feat: simple rag based chat bot that answers questions based of resume Built using langchain and flask Used gpt-4o

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import OpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+from langchain import hub
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+# openai model 
+llm = ChatOpenAI(model="gpt-4o")
+
+# Load and index the document once at startup
+loader = TextLoader("Data/Shiva.txt")
+
+
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+embeddings = OpenAIEmbeddings()
+vectorstore = FAISS.from_documents(texts, embeddings)
+
+retriever = vectorstore.as_retriever()
+
+prompt = hub.pull("rlm/rag-prompt")
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    data = request.json
+    question = data.get("question", "")
+    if not question:
+        return jsonify({"error": "No question provided"}), 400
+
+    response = rag_chain.invoke(question)
+    return jsonify({"answer": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
